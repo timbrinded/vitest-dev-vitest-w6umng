b5359762cb0380d4a28f16b402ed101f4f79ff3c
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-05-13 08:47:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitest-dev-vitest-w6umng
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitest-dev-vitest-zaoysv)
---
+++ test/5.test.ts
@@ -0,0 +1,184 @@
+import { assert, describe, expect, it } from 'vitest';
+import { setTimeout } from 'timers/promises';
+
+describe('suite name', async () => {
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('foo', async () => {
+    assert.equal(Math.sqrt(4), 2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('bar', async () => {
+    expect(1 + 1).eq(2);
+    await setTimeout(Math.random() * 1000);
+  });
+
+  it('snapshot', async () => {
+    expect({ foo: 'bar' }).toMatchSnapshot();
+    await setTimeout(Math.random() * 1000);
+  });
+});
